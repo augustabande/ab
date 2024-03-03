f027334b85cf805b90c3aa8267ff3da341c411ed
bandeau <augusta.bande@ext.ec.europa.eu>
2024-03-03 15:18:48 +0100

    fix: added netlify form

--- src/components/form/ContactForm.js
+++ src/components/form/ContactForm.js
@@ -103,7 +103,7 @@ class ContactForm extends Component {
 
     render() {
         return (
-            <form onSubmit={ this.submitForm } method="post" action="form.php" id="cf-1" className="contact-form" data-aos="fade-up" data-netlify="true">
+            <form id="cf-1" className="contact-form" data-aos="fade-up" data-netlify="true">
                 <div className="form-group form-group-sm" data-aos="fade-up">
                     <label htmlFor="cf-1-name" className="form-label-lg before">Name</label>
                     <input 
