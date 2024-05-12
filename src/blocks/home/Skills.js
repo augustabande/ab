9bbddaf87ba61644f92ea8caac7e8095d763b9b5
bandeau <augusta.bande@ext.ec.europa.eu>
2024-05-12 14:47:18 +0200

    added dynamic skills

--- src/blocks/home/Skills.js
+++ src/blocks/home/Skills.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import SkillsData from '../../data/home/skillsData.json';
 
 const Skills = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setIsVisible(true);
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    observer.observe(ref.current);
+
+    // Cleanup observer on component unmount
+    return () => observer.disconnect();
+  }, []);
   return (
-    <section id="skills" className="block spacer p-top-xl">
-      {/* My Skills */}
+    <section id="skills" className="block spacer p-top-xl" ref={ref}>
       <div className="wrapper">
         <div className="title" data-aos="fade-up">
           <h2>My Skills</h2>
@@ -20,7 +37,13 @@ const Skills = () => {
                       <span className="skill-percentage">{skill.percent}%</span>
                     </div>
                     <div className="progress">
-                      <div className="progress-bar" style={{ width: `${skill.percent}%` }}></div>
+                      <div 
+                        className="progress-bar"
+                        style={{ 
+                          width: isVisible ? `${skill.percent}%` : '0%',
+                          transition: 'width 2s ease' 
+                        }}
+                      ></div>
                     </div>
                   </div>
                 </div>
